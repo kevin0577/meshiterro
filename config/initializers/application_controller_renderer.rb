8cf45c8ccfcaafdbbc236cef2dc56acd419412d9
梶ヶ谷篤哉 <kajigayaatsuya@kajigayaatsuyanoMacBook-Air.local>
2020-03-14 00:08:22 +0900

    finish 5-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '28782ca488af11231014a3d5f7e5cbf4e81dfdf331b56245d4f2a2dbb3dc4125741da4ea1da1e53b0c6b0a7c4becf10230773bb44d6befddb5284462498f6fff'
