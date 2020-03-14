89655776db6381754825f1abb42b7acccf5a4aa3
梶ヶ谷篤哉 <kajigayaatsuya@kajigayaatsuyanoMacBook-Air.local>
2020-03-14 19:17:19 +0900

    finish 5-4

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -1,18 +1,22 @@
 class PostImagesController < ApplicationController
 	def new
-		@post_image = PostImage.new
+		@post_images = PostImage.page(params[:page]).reverse_order
 	end
-	def create
-		@post_image = PostImage.new(post_image_params)
-   		@post_image.user_id = current_user.id
-    	@post_image.save
-    	redirect_to post_images_path
-	end
+	 def create
+        @post_image = PostImage.new(post_image_params)
+        @post_image.user_id = current_user.id
+        if @post_image.save
+          redirect_to post_images_path
+        else
+          render :new
+        end
+     end
 	def index
 		@post_images = PostImage.all
 	end
 	def show
 		@post_image = PostImage.find(params[:id])
+		@post_comment = PostComment.new
 	end
 
 	private
